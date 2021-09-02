bb7dac7db4f6d9a2013820584065320e8fdce880
marvinwz <marvinhazebroek@gmail.com>
2021-09-02 10:42:55 +0200

    First Commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const https = require("https")
+
+const data = JSON.stringify({});
+
+const options = {
+    hostname: 'cloud.crownstone.rocks',
+    path: '/api/users/me',
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization': ''
+    }
+}
+//request
+const req = https.request(options, (res) => {
+        var body = [];
+
+        console.log("Status code: ", res.statusCode)
+
+        res.on('data', function (chunk) {
+            body += chunk;
+        })
+
+        res.on('end', function (chunk) {
+            console.log("Body :", JSON.parse(body));
+        })
+    })
+    .on("error", function (err) {
+        console.log("Error: " + err.message);
+    })
+req.write(data)
+req.end();
